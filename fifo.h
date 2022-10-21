04418f0e0273a968854db2bf8b6bfdf27621538f
lucasP243 <lucas.pinard@outlook.com>
2022-10-21 14:45:00 +0200

    Corrected fifo

--- fifo.h
+++ fifo.h
@@ -3,9 +3,10 @@
  * \author      Alexis Daley
  * \version     1.1
  * \date        02 february 2021
- * \brief       This files allow a user to create and manage a fifo buffer (circular buffer).
- * \details     This file gives the interfaces to be able to create and manage the fifo buffer.
- *              Concerning threads, the fifo is designed for one producer one consumer.
+ * \brief       This files allow a user to create and manage a fifo buffer
+ * (circular buffer). \details     This file gives the interfaces to be able to
+ * create and manage the fifo buffer. Concerning threads, the fifo is designed
+ * for one producer one consumer.
  *
  *              It implements following functions :
  *                  init : to init the fifo buffer
@@ -22,41 +23,45 @@
 #include <unistd.h>
 
 /* TODO : Can be adjusted depending on push/read frequency */
-#define FIFO_MAX_ITEMS  (256)           /* Max number of elements in fifo */
+#define FIFO_MAX_ITEMS (256) /* Max number of elements in fifo */
 
 /* Return codes */
-#define FIFO_FAILURE    (-1)            /* Bad parameters, NULL pointers or excluded ID */
-#define FIFO_DATA       (0)             /* "Normal" data item is returned or stored */
-#define FIFO_EMPTY      (1)             /* (read) FIFO is empty, no data returned */
-#define FIFO_OVERRUN    (2)             /* (push) FIFO is full, data item is rejected */
-#define FIFO_LOST       (3)             /* (read) 'sz_or_count' set to the count of lost items */
+#define FIFO_FAILURE (-1) /* Bad parameters, NULL pointers or excluded ID */
+#define FIFO_DATA (0)     /* "Normal" data item is returned or stored */
+#define FIFO_EMPTY (1)    /* (read) FIFO is empty, no data returned */
+#define FIFO_OVERRUN (2)  /* (push) FIFO is full, data item is rejected */
+#define FIFO_LOST                                                              \
+  (3) /* (read) 'sz_or_count' set to the count of lost items                   \
+       */
 
-typedef struct fifo_item_s
-{
-    /* TODO : fill the structure with willing elements */
+typedef struct fifo_item_s {
+  /* TODO : fill the structure with willing elements */
 } fifo_item_t;
 
 /**
- * \brief   Internal structure of the global FIFO header, used internally by FIFO implementation
- * \details Contains read and write index, but client does not need to know its content
+ * \brief   Internal structure of the global FIFO header, used internally by
+ * FIFO implementation \details Contains read and write index, but client does
+ * not need to know its content
  *
  */
-typedef struct hsi_fifo_s hsi_fifo_t;     /* forward declaration for the opaque structure */
+typedef struct hsi_fifo_s
+    hsi_fifo_t; /* forward declaration for the opaque structure */
 
 /**
  * \brief       Init fifo buffer (Should be used by producer thread)
  * \details     Initialize the fifo struct inside of allocated memory
  * \return      hsi_fifo_t : pointer to fifo structure
- *                            NULL in case of error: the size is too small or NULL pointer given
+ *                            NULL in case of error: the size is too small or
+ * NULL pointer given
  */
-hsi_fifo_t* fifo_init(void);
+hsi_fifo_t *fifo_init(void);
 
 /**
  * \brief       Get pointer on fifo struct (Should be used by consumer thread)
  * \details
  * \return      hsi_fifo_t : pointer to fifo structure
  */
-hsi_fifo_t* fifo_get_pointer(void);
+hsi_fifo_t *fifo_get_pointer(void);
 
 /**
  * \brief       Copy a data to fifo
@@ -67,25 +72,22 @@ hsi_fifo_t* fifo_get_pointer(void);
  *                        FIFO_OVERRUN (fifo full)
  *                        FIFO_FAILURE (pointer null or reserved ID gived)
  */
-int32_t fifo_push(hsi_fifo_t* p_fifo, const fifo_item_t* item);
+int32_t fifo_push(hsi_fifo_t *p_fifo, const fifo_item_t *item);
 
 /**
  * \brief       Read one data from the fifo (give a pointer to it)
  * \details     Give a pointer to the data which is in the write index.
- *              if there was rejected data on push, return special id (FIFO_LOST)
- * \param       p_fifo      : Pointer to the fifo object
- * \param[out]  item        : Pointer on item data to set
- * \return      int32_t : FIFO_DATA if a data is returned
- *                        FIFO_EMPTY if fifo is empty
- *                        FIFO_LOST if there was rejected data on push
- *                        FIFO_FAILURE if a given pointer is NULL
+ *              if there was rejected data on push, return special id
+ * (FIFO_LOST) \param       p_fifo      : Pointer to the fifo object \param[out]
+ * item        : Pointer on item data to set \return      int32_t : FIFO_DATA if
+ * a data is returned FIFO_EMPTY if fifo is empty FIFO_LOST if there was
+ * rejected data on push FIFO_FAILURE if a given pointer is NULL
  */
-int32_t fifo_read(hsi_fifo_t* p_fifo, fifo_item_t* item);
+int32_t fifo_read(hsi_fifo_t *p_fifo, fifo_item_t *item);
 
 /**
- * \brief       Place the read index to the next item, and call fifo_read with this new index
- * \details
- * \param       p_fifo      : Pointer to the fifo object
+ * \brief       Place the read index to the next item, and call fifo_read with
+ * this new index \details \param       p_fifo      : Pointer to the fifo object
  * \param[out]  item        : Pointer on item data to set
  *                            if ret value is  FIFO_LOST : number of data lost
  * \return      int32_t : FIFO_DATA if a data is returned
@@ -93,7 +95,6 @@ int32_t fifo_read(hsi_fifo_t* p_fifo, fifo_item_t* item);
  *                        FIFO_LOST if there was rejected data on push
  *                        FIFO_FAILURE - if a given pointer is NULL
  */
-int32_t fifo_next(hsi_fifo_t* p_fifo, fifo_item_t* item);
-
+int32_t fifo_next(hsi_fifo_t *p_fifo, fifo_item_t *item);
 
 #endif /* FIFO_H_ */
